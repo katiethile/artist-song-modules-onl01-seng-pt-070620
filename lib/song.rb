b8d9d4096709f21386a86a55a83efb096a6093a0
Katie Le <lethikatie@gmail.com>
2020-08-07 07:19:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,11 +17,7 @@ class Song
 
   def initialize
     @@songs << self
-  end
-
- # def self.find_by_name(name)
-  #  @@songs.detect{|a| a.name == name}
-  #end
+  end 
 
   def self.all
     @@songs
