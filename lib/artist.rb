cbc24877e0cf7aa688fd186bb21d2405bfbeec89
Katie Le <lethikatie@gmail.com>
2020-08-07 07:28:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,10 +13,8 @@ class Artist
   attr_accessor :name
   attr_reader :songs
   
-  extend Memorable::ClassMethods
-  extend Findable::ClassMethods
-  include Memorable::InstanceMethods
-  include Paramable::InstanceMethods
+extend Memorable::ClassMethods
+include Memorable::InstanceMethods
 
   @@artists = []
 
